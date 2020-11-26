fcb39a7f25411c2b3386674cd15c212a0ec2bcb7
Rulo Sanchez <luis.sanchez@kueski.com>
2020-11-25 22:58:25 -0700

    Improve code readability

--- omath_mota_rulo/data/covid_municipalities.py
+++ omath_mota_rulo/data/covid_municipalities.py
@@ -1,17 +1,41 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import requests
 import json
+import requests
+import os
+
+import pandas as pd
 
 import omath_mota_rulo.config as config
-urls = \
-'https://datamexico.org/api/data.jsonrecords?Municipality=%s,\
-%s:neighbors&cube=gobmx_covid_stats_mun&drilldowns=Time,\
-Municipality&measures=Accum+Cases,Daily+Cases,AVG+7+Days+Accum+Cases,\
-AVG+7+Days+Daily+Cases,Rate+Daily+Cases,Rate+Accum+Cases,\
-Days+from+50+Cases&parents=true&sparse=false&s=Casos positivos diarios&q=Fecha&r=withoutProcessOption'
 
-PATH = config.PRJ_DIR+'/data/raw/covid_mun/'
+
+DATA_PATH = os.path.join(
+    config.PRJ_DIR,
+    '/data/raw/covid_mun/')
+
+
+search_api = 'https://datamexico.org/api/data.jsonrecords?'
+
+munic_neigh = 'Municipality=%s,%s:neighbors&'
+
+params = '&'.join([
+    'cube=gobmx_covid_stats_mun',
+    'drilldowns=Time,Municipality',
+    ('measures='
+     'Accum+Cases,'
+     'Daily+Cases,'
+     'AVG+7+Days+Accum+Cases,'
+     'AVG+7+Days+Daily+Cases,'
+     'Rate+Daily+Cases,'
+     'Rate+Accum+Cases,'
+     'Days+from+50+Cases'),
+    'parents=true',
+    'sparse=false',
+    's=Casos positivos diarios',
+    'q=Fecha',
+    'r=withoutProcessOption'
+])
+
+urls = search_api + munic_neigh + params
 
 
 def load_data(url):
@@ -23,20 +47,27 @@ def load_data(url):
     if 'status' in data_dict.keys():
         return None
     data = pd.json_normalize(data_dict['data'])
-    print('Success')
+
+    if config.VERBOSE:
+        print('Success')
     return data
 
 
 def get_keys_muns(municipios):
     """
-
     """
     id_est = municipios['Cve_Ent'].unique()
     for _id_est in id_est:
-        id_mun = municipios[municipios['Cve_Ent'] == _id_est]['Cve_Mun'].unique()
+        id_mun = municipios[
+            municipios['Cve_Ent'] == _id_est
+        ]['Cve_Mun'].unique()
         for _id_mun in id_mun:
 
-            m = municipios[(municipios['Cve_Ent'] == _id_est) & (municipios['Cve_Mun'] == _id_mun)]
+            m = municipios[(
+                municipios['Cve_Ent'] == _id_est
+            ) & (
+                municipios['Cve_Mun'] == _id_mun)]
+
             name = m['Nom_Mun'].unique()[0]
 
             if _id_mun < 10:
@@ -57,10 +88,12 @@ def download():
 
     dict_mun = {}
     for mun in get_keys_muns(municipios):
-        print(mun)
+        if config.VERBOSE:
+            print(mun)
         key, name = mun
         dict_mun[name] = load_data(urls % (key, key))
-        dict_mun[name].to_csv(PATH + name + '.csv')
+        dict_mun[name].to_csv(
+            os.path.join(DATA_PATH, '%s.csv' % name))
 
 
 if __name__ == '__main__':
