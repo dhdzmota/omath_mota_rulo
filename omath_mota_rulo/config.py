778b009edacd0793add811601ec5ac2cfad23878
Rulo Sanchez <luis.sanchez@kueski.com>
2020-11-25 22:35:31 -0700

    Add script to get data from capacidad hospitalaria

---
+++ omath_mota_rulo/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+# Project path
+PRJ_DIR = '/'.join(os.path.abspath(__file__).split('/')[:-2])
+
+# Path to store data
+DATA_DIR = os.path.join(PRJ_DIR, 'data')
+
+# Flag used to silence prints
+VERBOSE = True
+
+# Number of jobs
+N_JOBS = 8
