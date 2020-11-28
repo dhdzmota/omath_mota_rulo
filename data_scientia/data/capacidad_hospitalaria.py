0ef3e1125284a0255a133ee2d310c04da1811536
Omath <omar@synx.co>
2020-11-28 00:29:08 -0600

    Add get_hospital_near_geo

--- data_scientia/data/capacidad_hospitalaria.py
+++ data_scientia/data/capacidad_hospitalaria.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 21:51:56 2020
+
+@author: Raul Sanchez-Vazquez
+"""
+
 import os
 import urllib3
 import json
 import pandas as pd
 
+from data_scientia.features.utils import distance_utils
 from data_scientia import config
 
 
 DATA_PATH = os.path.join(
     config.DATA_DIR,
     'raw/capacidad_hospitalaria.csv.gz')
+
+# Aux. variable use to keep the data in memory
+_DATA_HOSPITAL = None
+_HOSPITAL_LAT = None
+_HOSPITAL_LONG = None
 
 
 def download():
@@ -93,6 +105,56 @@ def get():
     data['longitude'] = data['coordenadas'].apply(lambda x: x[1])
 
     return data
+
+
+def load():
+    """
+    """
+    global _DATA_HOSPITAL
+    global _HOSPITAL_LAT
+    global _HOSPITAL_LONG
+
+    if _DATA_HOSPITAL is None:
+        _DATA_HOSPITAL = get()
+
+    if _HOSPITAL_LAT is None:
+        _HOSPITAL_LAT = _DATA_HOSPITAL.groupby('nombre_hospital')['latitude'].mean()
+
+    if _HOSPITAL_LONG is None:
+        _HOSPITAL_LONG = _DATA_HOSPITAL.groupby('nombre_hospital')['longitude'].mean()
+
+
+def get_hospital_near_geo(geo_points, max_meters=5e+3):
+
+    load()
+
+    global _DATA_HOSPITAL
+    global _HOSPITAL_LAT
+    global _HOSPITAL_LONG
+
+
+    hospitales_in_radio = []
+    for lat, long in geo_points:
+        lat_a = [lat] * _HOSPITAL_LAT.shape[0]
+        long_a = [long] * _HOSPITAL_LONG.shape[0]
+
+        dist = pd.Series(distance_utils.coord_dist(
+            lat_a=lat_a,
+            long_a=long_a,
+            lat_b=_HOSPITAL_LAT,
+            long_b=_HOSPITAL_LONG),
+            index=_HOSPITAL_LAT.index)
+
+        valid_dist = dist[(dist <= max_meters).values]
+        near_hospitals = _DATA_HOSPITAL[
+            _DATA_HOSPITAL['nombre_hospital'].isin(valid_dist.index)].copy()
+
+        near_hospitals.set_index('nombre_hospital', inplace=True)
+        near_hospitals['meters_to_geo_point'] = valid_dist
+
+        hospitales_in_radio.append(near_hospitals.index.unique().to_list())
+
+    return hospitales_in_radio
 
 
 if __name__ == '__main__':
